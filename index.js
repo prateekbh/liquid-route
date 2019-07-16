27456492789bcdf74a2834d1358e8069af664d17
Patrick Borowy <patrick.borowy@gmail.com>
2019-07-16 15:21:59 +0200

    added SlideRight animation definition in exports

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import LiquidRoute from './Components/LiquidRoute/LiquidRoute.jsx';
 import FadeAnimation from './Components/AnimationDefinations/fade';
 import PopAnimation from './Components/AnimationDefinations/pop';
 import SlideLeft from './Components/AnimationDefinations/slideLeft';
+import SlideRight from './Components/AnimationDefinations/slideRight';
 import AppearFromRightAnimation from './Components/AnimationDefinations/appearFromRight';
 import AppearFromBottomAnimation from './Components/AnimationDefinations/appearFromBottom';
 import PushAndSlide from './Components/AnimationDefinations/pushAndSlide';
 
 export default LiquidRoute;
-export {FadeAnimation, PopAnimation, SlideLeft, PushAndSlide, AppearFromBottomAnimation, AppearFromRightAnimation};
+export {FadeAnimation, PopAnimation, SlideLeft, PushAndSlide, AppearFromBottomAnimation, AppearFromRightAnimation, SlideRight};
